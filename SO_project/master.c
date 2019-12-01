c9519b6ee6741b7b4f0ff6ca183bf378c9b02869
lorenzo <lorenzo@ba3052a7-6ce0-41a4-a75b-8ecab7ed72f2>
2019-12-01 10:29:14 +0000

    working copy

--- SO_project/master.c
+++ SO_project/master.c
@@ -15,25 +15,55 @@
 #include "shared_res.h"
 #include "pawn.h"
 
-#define DEBUG
+//#define DEBUG
 
 int shmId;
 
 table *tableCreation(int base, int height) {
-    table *mytable;
+    table *myTable;
+    int i, j;
+
+    /*shared memory creation*/
     shmId = shmget(IPC_PRIVATE, sizeof(table), 0660);
     //TODO: come cazzo si usa IPC_EXCL?
     test_error();
-    mytable = shmat(shmId, NULL, 0);
+    myTable = shmat(shmId, NULL, 0);
     test_error();
+    //game table definition
+    myTable->base = base;
+    myTable->height = height;
+    myTable->matrix = malloc(sizeof(char *) * height);
+    myTable->semMatrix = malloc(sizeof(int) * height);
+    for (i = 0; i < base; ++i) {
+        myTable->matrix[i] = malloc(sizeof(char) * base);
+        myTable->semMatrix[i] = semget(IPC_PRIVATE, base, 0600);;
+        for (j = 0; j < height; ++j) {
+            myTable->matrix[i][j] = 'O';
+            semctl(myTable->semMatrix[i], j, SETVAL, 1);
+        }
+    }
 
-    mytable->base = base;
-    mytable->height = height;
-    return mytable;
+    return myTable;
 }
 
-void printState() {
+void printState(table myTable) {
+    int i, j, k;
 
+    for (i = 0; i < myTable.base; ++i) {
+        printf("----");
+    }
+    printf("\n");
+    for (i = 0; i < myTable.height; ++i) {
+        printf("|");
+        for (j = 0; j < myTable.base; ++j) {
+            printf(" %c |", myTable.matrix[i][j]);
+        }
+        printf("\n");
+        for (k = 0; k < myTable.base; ++k) {
+            printf("----");
+        }
+        printf("\n");
+    }
 }
 
 void flagsPositioning(table gameTable, int minFlag, int maxFlag, int roundScore) {
@@ -42,8 +72,8 @@ void flagsPositioning(table gameTable, int minFlag, int maxFlag, int roundScore)
     srand(getpid());
     flagNum = (rand() % (maxFlag - minFlag) + minFlag);
 
-    //todo: posizionamento delle bandiere sulla matrice schacchiera.
-    //i giocatori devono sapere il valore della bandiera?
+    /*todo: posizionamento delle bandiere sulla matrice schacchiera.
+    i giocatori devono sapere il valore della bandiera?*/
 
 }
 
@@ -59,7 +89,7 @@ pid_t *playersCreation(int numPlayers, int numPawn) {
             fprintf(stderr, "pid: %d , i'm a child\n", getpid());
 #endif
             playerBirth(numPawn);
-        } else //father actions
+        } else /*father actions*/
             players[i] = forkVal;
     }
 
@@ -77,17 +107,19 @@ int main(int argc, char **argv, char **envp) {
     env environment;
     table *sharedTable;
     pid_t *players;
+    int i;
 
+    setvbuf(stdout, NULL, _IONBF, 0);
     envReading(envp, &environment);
-#ifdef DEBUG
-    printf("%d", environment.SO_NUM_G);
-#endif
     sharedTable = tableCreation(environment.SO_BASE, environment.SO_ALTEZZA);
     //todo: così facendo (creazione delal tabella *prima* della creazione dei figli, essi avvranno nel loro stack 2 puntatori alla memoria, uno praticamente inutile però)
     playersCreation(environment.SO_NUM_G, environment.SO_NUM_G);
 
-    //cleanig what is left
+    /*cleanig what is left*/
     while (wait(NULL) != -1);
+    for (i = 0; i < environment.SO_ALTEZZA; ++i) {
+        semctl(sharedTable->semMatrix[i], 0, IPC_RMID);
+    }
     shmctl(shmId, IPC_RMID, NULL);
     shmdt(sharedTable);
     return 0;
--- SO_project/pawn.h
+++ SO_project/pawn.h
@@ -1,4 +1,10 @@
 #ifndef SO_PROJECT_PAWN_H
 #define SO_PROJECT_PAWN_H
 
+typedef struct {
+    int positionX, positionY;
+    int objectiveX, objectiveY;
+
+} pawn;
+
 #endif //SO_PROJECT_PAWN_H
